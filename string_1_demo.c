35ce4a1bea3b3a9c54006404da2020e129e033bf
普罗米修斯 <118711195+pengchao2022@users.noreply.github.com>
2026-04-05 23:16:49 +0800

    Create string_1_demo.c

---
+++ string_1_demo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main(void){
+    char s1[] = "This is the way.";
+
+    int length = strlen(s1);
+
+    printf("length = %d\n", length);
+
+    return 0;
+
+}
