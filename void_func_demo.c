d9b5ee0e36b9994735fd779950b75a44379bc55c
普罗米修斯 <118711195+pengchao2022@users.noreply.github.com>
2026-04-06 10:29:17 +0800

    Create void_func_demo.c

---
+++ void_func_demo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void print_two(float a, float b);
+
+int main(void){
+    float x = 2.5, y = 4.2;
+    print_two(x, y);
+
+    return 0;
+
+}
+
+void print_two(float a, float b){
+    printf("%f\n%f\n", a, b);
+}
