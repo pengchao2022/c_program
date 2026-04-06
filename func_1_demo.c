133bad80bf639d2e161a315354a540f198893546
普罗米修斯 <118711195+pengchao2022@users.noreply.github.com>
2026-04-06 09:50:06 +0800

    Create func_1_demo.c

---
+++ func_1_demo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int find_max(int a, int b){
+    if (a > b)
+    {
+        return a;
+    }else{
+        return b;
+    }
+    
+}
+
+int main(void){
+    int mx = find_max(5, 10);
+    printf("max is: %d\n", mx);
+}
