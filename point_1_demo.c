c7e3f558922ff65bc644127efa50c8f60fb1961b
普罗米修斯 <118711195+pengchao2022@users.noreply.github.com>
2026-04-06 14:09:24 +0800

    Update point_1_demo.c

--- point_1_demo.c
+++ point_1_demo.c
@@ -4,15 +4,20 @@ int main(void){
     int a = 10;
     int b = 5;
     int c = 20;
-    int *p;
+    int *p; // declare p is a pointer 
     p = &b;
 
     int *m;
     m = &a;
 
+    int *j; // declare j is a pointer 
+    j = &c;
+    *j = *j + 200;
+
     printf("P is memory address: %p\n", p);
     printf("m is the a's memory address: %p\n", m);
-    printf("&c is c's memory address: %p", &c);
+    printf("&c is c's memory address: %p\n", &c);
+    printf("*j is: %d\n", *j);
 
     return 0;
 
