d1aa246da1d825ede6a8f95a902e79570f6fbe3c
普罗米修斯 <118711195+pengchao2022@users.noreply.github.com>
2026-04-06 14:00:53 +0800

    Update point_1_demo.c

--- point_1_demo.c
+++ point_1_demo.c
@@ -3,6 +3,7 @@
 int main(void){
     int a = 10;
     int b = 5;
+    int c = 20;
     int *p;
     p = &b;
 
@@ -11,6 +12,7 @@ int main(void){
 
     printf("P is memory address: %p\n", p);
     printf("m is the a's memory address: %p\n", m);
+    printf("&c is c's memory address: %p", &c);
 
     return 0;
 
